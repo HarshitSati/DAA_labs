e2c6fdd99da25db8e84065aab8a0feac1a6d33bf
Harshit Sati <59113445+HarshitSati@users.noreply.github.com>
2021-07-17 17:59:43 +0530

    Create Wbinary_search.cpp

---
+++ Wbinary_search.cpp
@@ -0,0 +1,37 @@
+//time complexity of bin search O(logn)
+// t -> test case
+//high -> length of array
+#include<iostream>
+#include<vector>
+#include <cstdio>
+using namespace std;
+int binarySearch(vector<int> &v, int low,int high, int key){
+  int mid =0;
+  if(low ==high)return -1;
+  while(low<high){
+  mid = (high+low)/2;
+  if(key <v[mid])high = mid;
+  else if(key >v[mid])low = mid;
+  else return mid;
+  }
+return -1;
+}
+
+int main(){
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  freopen("binary_search.in","r",stdin);
+  freopen("binary_search.out","w",stdout);
+  int t;
+  cin>>t;
+  while(t--){
+    int high,key;
+    cin>>high;
+    cin>>key;
+    vector<int> vrr(high);
+    for(int i=0; i<high;i++){
+      cin>>vrr[i];
+    }
+    //int low =0;
+    cout<<binarySearch(vrr,0, high,key)<<"\n";
+  }
+}
