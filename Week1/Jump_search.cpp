9a8b724eeef8046590e9f6b96a10099439c2158c
Harshit Sati <59113445+HarshitSati@users.noreply.github.com>
2021-07-21 22:25:23 +0530

    Update Jump_search.cpp

--- Week1/Jump_search.cpp
+++ Week1/Jump_search.cpp
@@ -1,47 +1,37 @@
-//time complexity of bin search O(n/jump + line_search O(jump))
-// t -> test case
-//n -> length of array
-//
 #include<iostream>
 #include<vector>
 #include <cstdio>
 using namespace std;
-int linearSearch(vector<int> &vrr, int start, int end, int key){ //end - i+ jump or start + jump
-  while(start <= end){
-    //cout<<" "<<vrr[start]<<" ";
-    if(vrr[start] == key) return start;
-    start++;
+int binarySearch(vector<int> &v, int low,int high, int key,int &compare){
+  int mid =0;
+  if(low ==high)return -1;
+  while(low<high){
+  compare++;
+  mid = (high+low)/2;
+  if(key <v[mid])high = mid;
+  else if(key >v[mid])low = mid+1;
+  else return mid;
+//  cout<<compare<<" ";
   }
-//  cout<<"\n";
-  return -1;
-}
-int jumpSearch(vector<int> &vrr, int n,int key){
-  int jump =n/2 -1;
-  for(int i =0; i < n- jump; i+=jump){
-  //  cout<<vrr[i]<<" "<<vrr[i+jump]<<" ";
-    if(vrr[i] <= key&&key <= vrr[i+jump ]) return linearSearch(vrr, i, i+jump, key);
-  }
-  return -1;
+return -1;
 }
 
 int main(){
   ios_base::sync_with_stdio(0); cin.tie(0);
   freopen("binary_search.in","r",stdin);
-  freopen("jump_search.out","w",stdout);
+  freopen("binary_search.out","w",stdout);
   int t;
   cin>>t;
   while(t--){
-    int n,key;
-    cin>>n;
+    int high,key,compare=0;
+    cin>>high;
     cin>>key;
-    vector<int> vrr(n);
-    for(int i=0; i<n;i++){
+    vector<int> vrr(high);
+    for(int i=0; i<high;i++){
       cin>>vrr[i];
     }
     //int low =0;
-    int ans = jumpSearch(vrr, n,key);
-    //cout<<ans<<"\n";
-    if(ans) cout<<"Present "<<key<<" at "<<ans<<"\n";
-    else cout<<"Not Present "<<key;
+    int ans = binarySearch(vrr,0, high,key,compare);
+    cout<<ans<<" total comparisions "<<compare<<"\n";
   }
 }
